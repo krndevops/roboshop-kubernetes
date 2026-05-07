560c5d0655644904bb5f40d8fd50fdd22ee9c7ee
metal <metalheadkrn@gmail.com>
2026-05-07 13:03:44 +0530

    Cart Helm Changes

--- argo.sh
+++ argo.sh
@@ -1,4 +1,4 @@
-argocd login argocd.kdevops.online --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo) --insecure --grpc-web
+argocd login argocd.kdevops.online --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure --grpc-web
 for i in cart catalogue user payment shipping frontend ; do
 argocd app create $i --repo https://github.com/krndevops/$i --path k8s --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse --sync-policy none --grpc-web
 argocd app sync $i
