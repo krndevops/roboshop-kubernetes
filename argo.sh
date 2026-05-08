9252fbf98f30e0ee952df6d24603bb75aa7f119c
metal <metalheadkrn@gmail.com>
2026-05-08 15:40:44 +0530

    mODITING argo.sh to execute helm charts on argocd

--- argo.sh
+++ argo.sh
@@ -1,5 +1,16 @@
+#argocd login argocd.kdevops.online --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure --grpc-web
+#for i in cart catalogue user payment shipping frontend ; do
+#argocd app create $i --repo https://github.com/krndevops/$i --path k8s --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse --sync-policy none --grpc-web
+#argocd app sync $i
+#done
+
+
+aws eks update-kubeconfig --name dev-eks
 argocd login argocd.kdevops.online --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure --grpc-web
+
 for i in cart catalogue user payment shipping frontend ; do
-argocd app create $i --repo https://github.com/krndevops/$i --path k8s --dest-namespace default --dest-server https://kubernetes.default.svc --directory-recurse --sync-policy none --grpc-web
-argocd app sync $i
+
+  argocd app create $i --repo https://github.com/krndevops/$I --path helm --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values prod/$i.yaml
+  argocd app sync $i
+
 done
